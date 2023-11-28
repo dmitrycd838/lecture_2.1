634359da82540bb24242730e239c8b5496d643bf
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-11-28 13:33:27 +0300

    practice 2

--- two/Program.cs
+++ two/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти наибольшее из трех чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 23, 43, 65, 17, 78, 8, 21, 33 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+System.Console.WriteLine(max);
